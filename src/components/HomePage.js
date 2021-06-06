ff3cd4ab8cd24db069e1a97e823359344b96f4b3
Raymond Yeh <ray@geek.sg>
2021-06-06 13:29:59 +0800

    feat: Loading Metadata from Contract

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,33 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Contract, getDefaultProvider } from "ethers";
+import { config } from "../config";
+import abi from "../fixtures/abi.json";
+import axios from "axios";
 
-const MOCK_DATA = [
-  {
-    id: 0,
-    image:
-      "https://cloudflare-ipfs.com/ipfs/QmZGAPsFLA3KDPRWaACGgcoM1JuphuZ27Cg7KynqoC478u/robots/0.png",
-    name: "Robot #0",
-    description: "Pick Me. Pick Me.",
-    owner: "0x249bdb4499bd7c683664c149276c1d86108e2137",
-  },
-  {
-    id: 1,
-    image:
-      "https://cloudflare-ipfs.com/ipfs/QmZGAPsFLA3KDPRWaACGgcoM1JuphuZ27Cg7KynqoC478u/robots/1.png",
-    name: "Robot #1",
-    description: "Pick Me. Pick Me.",
-    owner: "0x249bdb4499bd7c683664c149276c1d86108e2137",
-  },
-  {
-    id: 2,
-    image:
-      "https://cloudflare-ipfs.com/ipfs/QmZGAPsFLA3KDPRWaACGgcoM1JuphuZ27Cg7KynqoC478u/robots/2.png",
-    name: "Robot #2",
-    description: "Pick Me. Pick Me.",
-    owner: "0x249bdb4499bd7c683664c149276c1d86108e2137",
-  },
-];
+const provider = getDefaultProvider("rinkeby", { alchemy: config.alchemyKey });
+const contract = new Contract(
+  "0x21f7FDEf49bFd7a45Dae776A31A6d90bEfe1059f",
+  abi,
+  provider
+);
+
+const formatIpfsUrl = (url) => {
+  return url.replace(/ipfs:\/\//g, "https://cloudflare-ipfs.com/");
+};
 
 export const HomePage = () => {
+  const [mintedNftState, setMintedNftState] = useState({
+    state: "UNINITIALIZED",
+  });
+
+  const loadRobotsData = async () => {
+    setMintedNftState({
+      state: "PENDING",
+    });
+    const totalSupply = await contract.totalSupply();
+    const ids = [...Array(totalSupply.toNumber()).keys()];
+    const deferredData = ids.map(async (id) => {
+      const ipfsUri = await contract.tokenURI(id);
+      const owner = await contract.ownerOf(id);
+      const formattedUri = formatIpfsUrl(ipfsUri);
+      const metadata = (await axios.get(formattedUri)).data;
+      const formattedImage = formatIpfsUrl(metadata.image);
+      return {
+        id,
+        name: metadata.name,
+        image: formattedImage,
+        description: metadata.description,
+        owner,
+      };
+    });
+    const data = await Promise.all(deferredData);
+    setMintedNftState({
+      state: "SUCCESS",
+      data,
+    });
+  };
+
+  useEffect(() => {
+    loadRobotsData();
+  }, []);
+
   const handlePurchase = () => {
     alert("Purchasing a Robot...");
   };
@@ -36,25 +59,32 @@ export const HomePage = () => {
     <div className="min-h-screen bg-gray-800">
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
         <div className="text-gray-100 text-6xl pt-28 pb-10">ROBOTS</div>
-        <div className="grid grid-cols-3 gap-4">
-          {MOCK_DATA.map(({ id, image, name, description, owner }) => {
-            return (
-              <div key={id} className="bg-white rounded p-2">
-                <img src={image} className="mx-auto p-4" alt={name} />
-                <div className="text-xl">{name}</div>
-                <div className="">{description}</div>
-                <hr className="my-4" />
-                <div className="text-left text-sm">Owned By:</div>
-                <div className="text-left text-xs">{owner}</div>
-              </div>
-            );
-          })}
-        </div>
+        {mintedNftState.state === "PENDING" && (
+          <div className="text-xl text-white">LOADING...</div>
+        )}
+        {mintedNftState.state === "SUCCESS" && (
+          <div className="grid grid-cols-3 gap-4">
+            {mintedNftState.data.map(
+              ({ id, image, name, description, owner }) => {
+                return (
+                  <div key={id} className="bg-white rounded p-2">
+                    <img src={image} className="mx-auto p-4" alt={name} />
+                    <div className="text-xl">{name}</div>
+                    <div className="">{description}</div>
+                    <hr className="my-4" />
+                    <div className="text-left text-sm">Owned By:</div>
+                    <div className="text-left text-xs">{owner}</div>
+                  </div>
+                );
+              }
+            )}
+          </div>
+        )}
         <div className="mt-12">
           <button
             onClick={handlePurchase}
             type="button"
-            class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
           >
             Buy My Robot
           </button>
