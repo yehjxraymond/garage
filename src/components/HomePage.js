70d2a818049270caeef5fa2931124e3a73835e8d
Raymond Yeh <ray@geek.sg>
2021-06-13 16:44:25 +0800

    feat: buy robot

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 const provider = getDefaultProvider("rinkeby", { alchemy: config.alchemyKey });
 const contract = new Contract(
-  "0x21f7FDEf49bFd7a45Dae776A31A6d90bEfe1059f",
+  "0x0Cc1bb3e59E9c7f76475e6C958892Cd3a3724e98",
   abi,
   provider
 );
@@ -19,6 +19,13 @@ export const HomePage = () => {
   const [mintedNftState, setMintedNftState] = useState({
     state: "UNINITIALIZED",
   });
+  const [purchaseState, setPurchaseState] = useState({
+    state: "UNINITIALIZED",
+  });
+  const modalVisible =
+    purchaseState.state === "PENDING_METAMASK" ||
+    purchaseState.state === "PENDING_SIGNER" ||
+    purchaseState.state === "PENDING_CONFIRMAION";
 
   const loadRobotsData = async () => {
     setMintedNftState({
@@ -56,6 +63,7 @@ export const HomePage = () => {
     if (typeof ethereum == "undefined") alert("Metamask is not detected");
 
     // Prompts Metamask to connect
+    setPurchaseState({ state: "PENDING_METAMASK" });
     await ethereum.enable();
 
     // Create new provider from Metamask
@@ -64,11 +72,22 @@ export const HomePage = () => {
     // Get the signer from Metamask
     const signer = provider.getSigner();
 
-    // Sends a simple transaction to an account
-    await signer.sendTransaction({
-      to: "0xA4bFA6B9819bc8857524eA5a6C691F045131307b",
-      value: utils.parseEther("0.01"),
-    });
+    // Create the contract instance
+    const contract = new Contract(
+      "0x0Cc1bb3e59E9c7f76475e6C958892Cd3a3724e98",
+      abi,
+      signer
+    );
+
+    // Call the purchase method
+    setPurchaseState({ state: "PENDING_SIGNER" });
+    const receipt = await contract.purchase({ value: utils.parseEther("1") });
+    setPurchaseState({ state: "PENDING_CONFIRMAION" });
+    const transaction = await receipt.wait();
+    setPurchaseState({ state: "SUCCESS", transaction });
+
+    // Reload the Robots
+    await loadRobotsData();
   };
 
   return (
@@ -106,6 +125,71 @@ export const HomePage = () => {
           </button>
         </div>
       </div>
+      {modalVisible && (
+        <div
+          className="fixed z-10 inset-0 overflow-y-auto"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+              aria-hidden="true"
+            />
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              ​
+            </span>
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+              <div>
+                <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-yellow-100">
+                  <svg
+                    className="h-6 w-6 text-yellow-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                    />
+                  </svg>
+                </div>
+                <div className="mt-3 text-center sm:mt-5">
+                  <h3
+                    className="text-lg leading-6 font-medium text-gray-900"
+                    id="modal-title"
+                  >
+                    {purchaseState.state === "PENDING_METAMASK" &&
+                      "Connecting Metamask..."}
+                    {purchaseState.state === "PENDING_SIGNER" &&
+                      "Waiting for Signed Transaction"}
+                    {purchaseState.state === "PENDING_CONFIRMAION" &&
+                      "Waiting for Block Confirmation"}
+                  </h3>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      {purchaseState.state === "PENDING_METAMASK" &&
+                        "Allow Metamask to connect to this application in the extension."}
+                      {purchaseState.state === "PENDING_SIGNER" &&
+                        "Approve the purchase transaction within the Metamask extension"}
+                      {purchaseState.state === "PENDING_CONFIRMAION" &&
+                        "Transaction has been sent to the blockchain. Please wait while the transaction is being confirmed."}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
